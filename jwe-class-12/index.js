cb787b214d59ac6ea4c5c5338afcf3397ef0225f
Sachin <sachinkshuklaoo7@gmail.com>
2023-06-02 21:36:20 +0530

    Class 13 Completed

---
+++ jwe-class-12/index.js
@@ -0,0 +1,55 @@
+// // Objects
+// const obj = {
+//     name: "Sachin",
+//     age: 70,
+//     gender: "M"
+// }
+
+// // Using Dot Operator => When you know the keys already
+// console.log(typeof obj)
+
+// // Using Square Braces => When you don't know the keys already
+// console.log(obj['name'])
+
+// // Dynamic key
+// const key = 'gender'
+
+// if (obj[key]) { // obj[key] => obj['gender']
+//     console.log("abc key exists in obj")
+// } else {
+//     console.log("Key doesn't exist", obj.key)
+// }
+
+// // Arrays
+// const students = ["Ramesh", "Suresh", "Mohan"]
+
+// console.log(students)
+
+// // Get values using index
+// console.log(students[0])
+
+// console.log(students.length)
+
+// Functions
+// Send mail
+// Call Someone
+// The function of a Pen is to write
+
+// A function that adds two numbers
+function sum(a, b) {
+    const add = a + b
+
+    return obj
+}
+
+console.log(sum(3, 6)) // Calling/Invoking a function
+
+const sumAsArrowFunction = (a, b) => {
+    return a + b
+}
+
+console.log(sumAsArrowFunction(3, 6))
+
+// everywhere if it has dot it means it's an object.
+// Array - Multiple items of similiar features together
+// Function - Perform an operation
---
+++ jwe-class-12/readme.md
@@ -0,0 +1,1 @@
+Whiteboard - https://excalidraw.com/#json=sX7-03iPoHD9RQ4v24Wam,SbHeetq8Hr80yBmN-LH1Jg
